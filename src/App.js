cf37a68be7f0014b7ccdb37f3df1102e145e275d
sredny <sredny.nevermind92@gmail.com>
2017-03-10 18:24:16 -0300

    child component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,22 @@ import React from 'react';
 
 
 class App extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            txt: 'this is the state txt'
+        }
+    }
+
+    update(e){
+        this.setState({txt:e.target.value});
+    }
+
     render(){
-        return <h1>{this.props.text}</h1>
+        return <div>
+                    <h1>{this.state.txt}</h1>
+                    <Widget update={this.update.bind(this)}/>
+                </div>
         //return React.createElement('h1',null, 'Hello creating from React!')
     }
 }
@@ -15,6 +29,8 @@ App.propTypes = {
 App.defaultProps={
     text:"this is react!! default prop"
 }
+
+const Widget= (props) =>  <input type="text" onChange={props.update}/>
 
 //const App= () => <h1>Return stateless component</h1>
 export default App
