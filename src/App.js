e671af5e2f057dda2bb4ac78cf0b21407219d043
sredny <sredny.nevermind92@gmail.com>
2017-03-10 18:37:15 -0300

    nested components

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends React.Component{
         return <div>
                     <h1>{this.state.txt}</h1>
                     <Widget update={this.update.bind(this)}/>
+                    <Button>I <Heart/> React</Button>
                 </div>
         //return React.createElement('h1',null, 'Hello creating from React!')
     }
@@ -31,6 +32,12 @@ App.defaultProps={
 }
 
 const Widget= (props) =>  <input type="text" onChange={props.update}/>
-
+const Button= (props) =>  <button>{props.children}</button>
 //const App= () => <h1>Return stateless component</h1>
+
+class Heart extends React.Component{
+    render(){
+        return <span>&hearts;</span>
+    }
+}
 export default App
