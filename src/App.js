3cb2659b5a095d95bffb6117191d5bd042c4fb52
sredny <sredny.nevermind92@gmail.com>
2017-03-10 17:56:22 -0300

    properties

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
-/*
+
 class App extends React.Component{
     render(){
-        return React.createElement('h1',null, 'Hello creating from React!')
+        return <h1>{this.props.text}</h1>
+        //return React.createElement('h1',null, 'Hello creating from React!')
     }
-}*/
+}
 
-const App= () => <h1>Return stateless component</h1>
+App.propTypes = {
+    txt: React.PropTypes.string,
+}
+
+App.defaultProps={
+    text:"this is react!! default prop"
+}
+
+//const App= () => <h1>Return stateless component</h1>
 export default App
--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <App />,
+  <App text="This is the prop text" cat={5}/>,
   document.getElementById('root')
 );
